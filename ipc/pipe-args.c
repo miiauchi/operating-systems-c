fd1785ea4cb193063771054cd3c4489c5c6f32b9
miiauchi <michelleauchen@gmail.com>
2026-07-27 15:01:04 -0700

    Create pipe-args.c

---
+++ ipc/pipe-args.c
@@ -0,0 +1,48 @@
+/*
+ * Name: Michelle Chen
+ * Title: IPC - Pipe with 1-byte buffer
+ * Description: A writer sends command-line arguments through a pipe; the reader
+ *              uses a 1-byte buffer and relies on the count returned by read()
+ *              to process the data correctly.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main(int argc,char *argv[]){
+	int  fds[2];
+	char buff[1];
+	int count;
+	int i;
+	pipe(fds);
+	if (fork()==0){
+		printf("\nWriter on the upstream end of the pipe -> %d arguments \n",argc);
+		close(fds[0]);
+		for(i=0;i<argc;i++){
+			write(fds[1],argv[i],strlen(argv[i]));
+		}
+		exit(0);
+	}
+	else if(fork()==0){
+		printf("\nReader on the downstream end of the pipe \n");
+		close(fds[1]);
+		while((count=read(fds[0],buff,1))>0){
+			for(i=0;i<count;i++){
+				write(1,buff+i,1);
+				write(1," ",1);
+			}
+			printf("\n");
+		}
+		exit(0);
+	}
+	else{
+		close(fds[0]);
+		close(fds[1]);
+		wait(0);
+		wait(0);
+	}
+	return 0;
+}
